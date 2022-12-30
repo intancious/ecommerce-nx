fef262c85e6c47f49bfb54f8ae1b3d5c19a95590
intancious98 <intancious98@gmail.com>
2022-12-30 18:21:31 +0700

    tes

--- nuxt.config.js
+++ nuxt.config.js
@@ -150,7 +150,8 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://localhost:8000'
+    // baseURL: 'http://localhost:8000'
+    baseURL: 'http://ecommerce.intancious.my.id'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
