246d9b7fbf78dbf377657ec4c2d384f8a595b597
intancious98 <intancious98@gmail.com>
2023-02-10 10:31:30 +0800

    initial commit first

--- store/admin/invoice.js
+++ store/admin/invoice.js
@@ -1,93 +1,76 @@
 //state
 export const state = () => ({
+  //invoices
+  invoices: [],
 
-    //invoices
-    invoices: [],
+  //page
+  page: 1,
 
-    //page
-    page: 1,
-
-    //invoice
-    invoice: {}
-
-})
+  //invoice
+  invoice: {}
+});
 
 //mutations
 export const mutations = {
+  //mutation "SET_INVOICES_DATA"
+  SET_INVOICES_DATA(state, payload) {
+    //set value state "invoices"
+    state.invoices = payload;
+  },
 
-    //mutation "SET_INVOICES_DATA"
-    SET_INVOICES_DATA(state, payload) {
+  //mutation "SET_PAGE"
+  SET_PAGE(state, payload) {
+    //set value state "page"
+    state.page = payload;
+  },
 
-        //set value state "invoices"
-        state.invoices = payload
-    },
-
-    //mutation "SET_PAGE"
-    SET_PAGE(state, payload) {
-
-        //set value state "page"
-        state.page = payload
-    },
-
-    //mutation "SET_INVOICE_DATA"
-    SET_INVOICE_DATA(state, payload) {
-
-        //set value state "invoice"
-        state.invoice = payload
-    },
-}
+  //mutation "SET_INVOICE_DATA"
+  SET_INVOICE_DATA(state, payload) {
+    //set value state "invoice"
+    state.invoice = payload;
+  }
+};
 
 //actions
 export const actions = {
+  //get invoices data
+  getInvoicesData({ commit, state }, payload) {
+    //search
+    let search = payload ? payload : "";
 
-    //get invoices data
-    getInvoicesData({ commit, state }, payload) {
+    //set promise
+    return new Promise((resolve, reject) => {
+      //fetching Rest API "/api/admin/invoices" with method "GET"
+      this.$axios
+        .get(`/api/admin/invoices?q=${search}&page=${state.page}`)
 
-        //search
-        let search = payload ? payload : ''
+        //success
+        .then(response => {
+          //commit ti mutation "SET_INVOICES_DATA"
+          commit("SET_INVOICES_DATA", response.data.data);
 
-        //set promise
-        return new Promise((resolve, reject) => {
+          //resolve promise
+          resolve();
+        });
+    });
+  },
 
-            //fetching Rest API "/api/admin/invoices" with method "GET"
-            this.$axios.get(`/api/admin/invoices?q=${search}&page=${state.page}`)
-            
-            //success
-            .then((response) => {
+  //get detail invoice
+  getDetailInvoice({ commit }, payload) {
+    //set promise
+    return new Promise((resolve, reject) => {
+      //get to Rest API "/api/admin/invoices/:id" with method "GET"
+      this.$axios
+        .get(`/api/admin/invoices/${payload}`)
 
-                //commit ti mutation "SET_INVOICES_DATA"
-                commit('SET_INVOICES_DATA', response.data.data)
+        //success
+        .then(response => {
+          //commit to mutation "SET_INVOICE_DATA"
+          commit("SET_INVOICE_DATA", response.data.data);
 
-                //resolve promise
-                resolve()
-            })
-
-        })
-
-    },
-
-    //get detail invoice
-    getDetailInvoice({ commit }, payload) {
-
-        //set promise
-        return new Promise((resolve, reject) => {
-
-            //get to Rest API "/api/admin/invoices/:id" with method "GET"
-            this.$axios.get(`/api/admin/invoices/${payload}`)
-
-            //success
-            .then(response => {
-
-                //commit to mutation "SET_INVOICE_DATA"
-                commit('SET_INVOICE_DATA', response.data.data)
-
-                //resolve promise
-                resolve()
-
-            })
-
-        })
-
-    },
-
-}
+          //resolve promise
+          resolve();
+        });
+    });
+  }
+};
